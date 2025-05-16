417ef8093a838c5d044980f2fa8c9217860fe101
arthur <arthur.henryck@hotmail.com>
2025-05-16 11:52:42 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState("texto")
+  const [count, setCount] = useState(1)
   const handleClick = () => {
-    alert(count)
+    setCount(count * 2)
+    console.log(count)
   }
 
   return (
     <>
       <div className="card">
         <button onClick={handleClick}>Clique Aqui</button>
+        {count}
       </div>
     </>
   )
